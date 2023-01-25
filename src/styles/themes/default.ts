e869a1f740a2fb3b3c99bf1938bab7049be3ba2d
Andreia Souza <fsouza.andreia@gmail.com>
2023-01-25 22:17:37 +0000

    :tada: Initial Commit

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,41 @@
+export const defaultTheme = {
+  brand: {
+    yellow: "#DBAC2C",
+    yellowDark: "#C47F17",
+    yellowLight: "#F1E9C9",
+    purple: "#8047F8",
+    purpleDark: "#4B2995",
+    purpleLight: "#EBE5F9",
+  },
+  base: {
+    white: "#FFFFFF",
+    background: "#FAFAFA",
+    card: "#F3F2F2",
+    input: "#EDEDED",
+    button: "#E6E5E5",
+    hover: "#D7D5D5",
+    label: "#8D8686",
+    text: "#574F4D",
+    subtitle: "#403937",
+    title: "#272221",
+  },
+  font: {
+    main: `'Roboto', sans-serif`,
+    display: `'Baloo 2', cursive`,
+  },
+  fontSize: {
+    10: "0.625rem",
+    12: "0.75rem",
+    14: "0.875rem",
+    16: "1rem",
+    18: "1.125rem",
+    20: "1.25rem",
+    24: "1.5rem",
+    32: "2rem",
+    48: "3rem",
+  },
+  lineHeight: {
+    130: "130%",
+    160: "160%",
+  },
+};
